55606354321e1ff3b6a78904b653f79973a7b26d
as45d001-ctrl <as45d001@gmail.com>
2026-06-26 15:28:01 +0800

    更新網頁報告

--- sw.js
+++ sw.js
@@ -1,9 +1,14 @@
-const CACHE = 'trend-winner-v1';
-const ASSETS = ['/', '/index.html', '/manifest.json'];
+const CACHE = 'trend-winner-v2';
+const ASSETS = [
+  './index.html',
+  './manifest.json'
+];
 
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
+    caches.open(CACHE)
+      .then(c => c.addAll(ASSETS))
+      .then(() => self.skipWaiting())
   );
 });
 
@@ -18,13 +23,12 @@ self.addEventListener('activate', e => {
 self.addEventListener('fetch', e => {
   e.respondWith(
     caches.match(e.request).then(cached => {
-      if (cached) return cached;
-      return fetch(e.request).then(res => {
-        if (!res || res.status !== 200) return res;
+      return cached || fetch(e.request).then(res => {
+        if (!res || res.status !== 200 || res.type !== 'basic') return res;
         const clone = res.clone();
         caches.open(CACHE).then(c => c.put(e.request, clone));
         return res;
-      }).catch(() => cached);
+      }).catch(() => caches.match('./index.html'));
     })
   );
 });
